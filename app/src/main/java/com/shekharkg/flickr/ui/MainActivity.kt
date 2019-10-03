4f91a271ee70cb77148376c55b5741e6b5e53785
ShekharKG <shekhar114@gmail.com>
2019-10-03 22:40:46 +0530

    feat: add injector util, inject view model

--- app/src/main/java/com/shekharkg/flickr/ui/MainActivity.kt
+++ app/src/main/java/com/shekharkg/flickr/ui/MainActivity.kt
@@ -2,9 +2,16 @@ package com.shekharkg.flickr.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.shekharkg.flickr.R
+import com.shekharkg.flickr.utils.InjectorUtils
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel: MainViewModel by lazy {
+        ViewModelProviders.of(this, InjectorUtils.provideMainViewModelFactory(application))
+            .get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
