dd97e2fcd4cef40116d223ebbe24e2b58b0547b7
ShekharKG <shekhar114@gmail.com>
2019-10-03 20:44:07 +0530

    refactor: mvvm structure

--- app/src/main/java/com/shekharkg/flickr/ui/MainActivity.kt
+++ app/src/main/java/com/shekharkg/flickr/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.shekharkg.flickr
+package com.shekharkg.flickr.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shekharkg.flickr.R
 
 class MainActivity : AppCompatActivity() {
 
